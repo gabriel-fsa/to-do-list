f3e38b4d7837bc987ee7e62d57f959ba53cbf8b1
Gabriel <gabriel_fsa@live.com>
2022-03-06 23:14:20 -0300

    chore: change the env

--- src/services/axios/api.ts
+++ src/services/axios/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: process.env.BACKEND_URL || 'http://localhost:3001/',
+  baseURL: process.env.REACT_APP_BACKEND_URL || 'http://localhost:3001/',
   // baseURL: 'https://todo-list-gabriel.herokuapp.com/',
 })
