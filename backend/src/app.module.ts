5beda9e908c3f0a3d08d1e16c34a72b7b0a80de6
Gabriel <gabriel_fsa@live.com>
2022-03-06 21:49:58 -0300

    feat: add the nestjs framework

---
+++ backend/src/app.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common'
+import { TodosModule } from './todos/todos.module'
+import { TaskModule } from './task/task.module'
+
+@Module({
+  imports: [],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
