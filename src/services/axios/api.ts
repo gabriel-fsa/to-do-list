af0680549523934a21572e8373013590d9dde10b
Gabriel <gabriel_fsa@live.com>
2022-03-06 23:25:24 -0300

    chore: add the url from backend

--- src/services/axios/api.ts
+++ src/services/axios/api.ts
@@ -1,6 +1,9 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: process.env.REACT_APP_BACKEND_URL || 'http://localhost:3001/',
+  baseURL:
+    process.env.REACT_APP_BACKEND_URL ||
+    'https://todo-list-portifolio.herokuapp.com/' ||
+    'http://localhost:3001/',
   // baseURL: 'https://todo-list-gabriel.herokuapp.com/',
 })
