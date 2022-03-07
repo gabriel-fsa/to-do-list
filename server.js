a631b59fb309195978956cac8cf497c2ea126e66
Gabriel <gabriel_fsa@live.com>
2022-03-06 23:08:13 -0300

    chore: add dotenv

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const { resolve } = require('path')
-
+require('dotenv').config()
 const app = express()
 
 app.use('/', express.static(resolve(__dirname, './build')))
