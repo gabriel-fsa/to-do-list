fea1d574a678bc7a74b1ce739a5171c9007356f9
Gabriel <gabriel_fsa@live.com>
2022-03-06 23:36:23 -0300

    chore: add server file

---
+++ server.js
@@ -0,0 +1,11 @@
+const path = require('path')
+const express = require('express')
+require('dotenv').config()
+const app = express()
+const publicPath = path.join(__dirname, 'build', 'public')
+app.use(express.static(publicPath))
+
+app.listen(process.env.PORT || 3000, (err) => {
+  if (err) return console.error(err)
+  console.log(`Server is running on ${process.env.PORT}`)
+})
