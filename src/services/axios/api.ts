c1db69620274fd52ce7d65a8cbd048a22a322d4c
Gabriel <gabriel_fsa@live.com>
2022-03-08 22:50:49 -0300

    refactor: change the url api to env variable

--- src/services/axios/api.ts
+++ src/services/axios/api.ts
@@ -3,7 +3,5 @@ import axios from 'axios'
 export const api = axios.create({
   baseURL:
     process.env.REACT_APP_BACKEND_URL ||
-    'https://todo-list-portifolio.herokuapp.com/' ||
-    'http://localhost:3001/',
-  // baseURL: 'https://todo-list-gabriel.herokuapp.com/',
+    'http://localhost:3001/' ||
 })
