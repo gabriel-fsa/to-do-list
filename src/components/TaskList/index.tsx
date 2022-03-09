4b195cd71a87dafadb984695773c29595e1ba123
Gabriel <gabriel_fsa@live.com>
2022-03-08 22:46:47 -0300

    refactor: change the taskItem and add styles

--- src/components/TaskList/index.tsx
+++ src/components/TaskList/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useTodo } from '../../hook/useTodo';
 import InputForm from '../Form';
-import TaskItem from '../TaskItem';
+import TableItem from '../ListItem';
 
 import { Container } from './styles';
 
@@ -9,14 +9,44 @@ interface TaskListProps {
 }
 
 function TaskList({ }: TaskListProps) {
-  const { todos, selectedTodo } = useTodo()
+  const {
+    todos,
+    selectedTodo,
+    setEdit,
+    handleDeleteTask,
+    toggleCompleteTask,
+  } = useTodo()
 
   const tasks = todos.find((todo) => todo.id === Number(selectedTodo))?.tasks
 
+  function handleEditButton(todoId: number, taskId: number, mode: 'task' | 'todo' = 'task') {
+    return () => setEdit({
+      mode,
+      todoId: Number(selectedTodo),
+      taskId: taskId,
+      message: todos
+        ?.find((todo) => todo.id === Number(selectedTodo))?.tasks
+        ?.find((task) => task.id === taskId)?.name || ''
+    }
+    )
+  }
+  function handleDeleteButton(todoId: number, taskId: number) {
+    return () => handleDeleteTask(todoId, taskId)
+
+  }
+  function handleCheckbox(taskId: number) {
+    return () => toggleCompleteTask(Number(selectedTodo), taskId)
+  }
+
   return (
     <Container>
-      <InputForm isInTaskPage />
-      {tasks?.map((task) => <TaskItem key={task.id} task={task} />)}
+      {tasks?.map((task) => <TableItem
+        key={task.id}
+
+        taskItem={task}
+        onDeleteButton={handleDeleteButton(Number(selectedTodo), task.id)}
+        onEditButton={handleEditButton(Number(selectedTodo), task.id)}
+        onCheckboxClicked={handleCheckbox(task.id)} />)}
     </Container>
   );
 };
