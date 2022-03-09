33b99e04872e3b74b3030c605bdf5e07a22db6a5
Gabriel <gabriel_fsa@live.com>
2022-03-09 10:21:45 -0300

    fix: sintaxe error

--- src/services/axios/api.ts
+++ src/services/axios/api.ts
@@ -1,7 +1,5 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL:
-    process.env.REACT_APP_BACKEND_URL ||
-    'http://localhost:3001/' ||
+  baseURL: process.env.REACT_APP_BACKEND_URL || 'http://localhost:3001/',
 })
